b5a712755b83d7e4a0afc1183a4bed4d3480a85f
Sushant Kate <47480733+Sushant98K@users.noreply.github.com>
2025-05-23 01:52:33 +0530

    Created Dashboard page layout and created API call to fetch dashboard data

--- frontend/src/pages/Admin/Dashboard.jsx
+++ frontend/src/pages/Admin/Dashboard.jsx
@@ -1,16 +1,42 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useUserAuth } from "../../hooks/useUserAuth";
 import { UserContext } from "../../context/userContext";
+import DashboardLayout from "../../components/Layouts/DashboardLayout";
+import { useNavigate } from "react-router-dom";
+import axiosInstance from "../../utils/axiosInstance";
+import { API_PATHS } from "../../utils/apiPath";
 
 const Dashboard = () => {
   useUserAuth();
 
-  const {user} = useContext(UserContext)
+  const { user } = useContext(UserContext)
+  
+  const navigate = useNavigate()
+  const [dashboardData, setDashboardData] = useState(null);
+  const [pieChartData, setPieChartData] = useState([]);
+  const [barChartData, setBarChartData] = useState([]);
+
+  const getDashboardData = async () => {
+    try {
+      const response = await axiosInstance.get(
+        API_PATHS.TASKS.GET_DASHBOARD_DATA
+      )
+      if (response.data) {
+        setDashboardData(response.data)
+      }
+    } catch (error) {
+      console.error("Error fetching users:", error);
+    }
+  }
+
+  useEffect(() => {
+    getDashboardData()
+
+    return ()=>{}
+  },[])
 
   return (
-    <div>
-      Admin Dashboard
-    </div>
+    <DashboardLayout activeMenu='Dashboard'>Admin Dashboard</DashboardLayout>
   )
 };
 
