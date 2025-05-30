490a4bdfc0aa24ca678d0060e9287cadbbcbf246
Sushant Kate <47480733+Sushant98K@users.noreply.github.com>
2025-05-30 21:08:03 +0530

    Select DropDown Functionality created

---
+++ frontend/src/components/Inputs/SelectDropdown.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { LuChevronDown } from "react-icons/lu";
+
+const SelectDropdown = ({ options, value, onChange, placeholder }) => {
+  return <div>SelectDropdown</div>;
+};
+
+export default SelectDropdown;
--- frontend/src/pages/Admin/CreateTask.jsx
+++ frontend/src/pages/Admin/CreateTask.jsx
@@ -4,6 +4,8 @@ import moment from "moment";
 import { LuTrash2 } from "react-icons/lu";
 import DashboardLayout from "../../components/Layouts/DashboardLayout";
 import { useLocation, useNavigate } from "react-router-dom";
+import { PRIORITY_DATA } from "../../utils/data";
+import SelectDropdown from "../../components/Inputs/SelectDropdown";
 
 const CreateTask = () => {
   const location = useLocation();
@@ -20,14 +22,14 @@ const CreateTask = () => {
     attachments: [],
   });
 
-  const [error, setError] = useState('');
+  const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
 
   const [openDeleteAlert, setOpenDeleteAlert] = useState(false);
 
   const handleValueChange = (key, value) => {
-    setTaskData((prevData) => ({ ...prevData, [key]: value }))
-  }
+    setTaskData((prevData) => ({ ...prevData, [key]: value }));
+  };
 
   const clearData = () => {
     // Reset Form
@@ -40,24 +42,89 @@ const CreateTask = () => {
       todoChecklist: [],
       attachments: [],
     });
-  }
+  };
 
   // Create Task
-  const createTask = async () => { }
-  
-  // Update Task
-  const updateTask = async () => { }
-  
-  const handleSubmit = async () => { }
-  
-  // Get Task info by id
-  const getTaskDetailsByID = async () => { }
-  
-  // Delete Task
-  cosnt deleteTask = async () => { }
-  
+  const createTask = async () => {};
 
-  return <DashboardLayout activeMenu="Create Task">CreateTask</DashboardLayout>;
+  // Update Task
+  const updateTask = async () => {};
+
+  const handleSubmit = async () => {};
+
+  // Get Task info by id
+  const getTaskDetailsByID = async () => {};
+
+  // Delete Task
+  const deleteTask = async () => {};
+
+  return (
+    <DashboardLayout activeMenu="Create Task">
+      <div className="mt-5">
+        <div className="grid grid-cols-1 md:grid-cols-4 mt-4">
+          <div className="form-card col-span-3">
+            <div className="flex items-center justify-between">
+              <h2 className="text-xl md:text-xl font-bold">
+                {taskId ? "Update Task" : "Create Task"}
+              </h2>
+
+              {taskId && (
+                <button
+                  className="flex items-center gap-1.5 text-[13px] font-medium text-rose-500 bg-rose-50 rounded px-2 py-1 border border-rose-100 hover:border-rose-300 cursor-pointer"
+                  onClick={() => setOpenDeleteAlert(true)}
+                >
+                  <LuTrash2 className="" /> Delete
+                </button>
+              )}
+            </div>
+
+            <div className="mt-4">
+              <label className="text-xs font-medium text-slate-600">
+                Task Title
+              </label>
+
+              <input
+                placeholder="Create App UI"
+                className="form-input"
+                value={taskData.title}
+                onChange={({ target }) =>
+                  handleValueChange("title", target.value)
+                }
+              />
+            </div>
+
+            <div className="mt-3">
+              <label className="text-xs font-medium text-slate-600">Description</label>
+
+              <textarea
+                placeholder="Describe Task"
+                className="form-input"
+                rows={4}
+                value={taskData.description}
+                onChange={({ target }) =>
+                  handleValueChange("description", target.value)
+                }
+              />
+            </div>
+
+            <div className="grid grid-cols-12 gap-4 mt-2">
+              <div className="col-span-6 md:col-span-4">
+                <label className="text-xs font-medium text-slate-600">Priority</label>
+
+                <SelectDropdown
+                  options={PRIORITY_DATA}
+                  value={taskData.priority}
+                  onChange={(value) => handleValueChange("priority", value)}
+                  placeholder="Select Priority"
+                />
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </DashboardLayout>
+  );
 };
 
 export default CreateTask;
