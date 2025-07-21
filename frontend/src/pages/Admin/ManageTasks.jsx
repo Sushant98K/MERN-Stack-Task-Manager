69badceceb5bf0f41928c6e741aac8386a502ae1
Sushant Kate <47480733+Sushant98K@users.noreply.github.com>
2025-07-21 22:53:31 +0530

    Displaying Task Cards in a Grid

--- frontend/src/pages/Admin/ManageTasks.jsx
+++ frontend/src/pages/Admin/ManageTasks.jsx
@@ -5,6 +5,7 @@ import axiosInstance from "../../utils/axiosInstance";
 import { API_PATHS } from "../../utils/apiPath";
 import { LuFileSpreadsheet } from "react-icons/lu";
 import TaskStatusTabs from "../../components/TaskStatusTabs";
+import TaskCard from "../../components/Cards/TaskCard";
 
 const ManageTasks = () => {
   const [allTasks, setAllTasks] = useState([]);
@@ -15,15 +16,14 @@ const ManageTasks = () => {
   const navigate = useNavigate();
 
   const getAllTasks = async () => {
-
     try {
       const response = await axiosInstance.get(API_PATHS.TASKS.GET_ALL_TASKS, {
         params: {
           status: filterStatus === "All" ? "" : filterStatus,
         },
-      })
+      });
 
-      setAllTasks(response.data?.tasks?.length > 0 ? response.data.tasks : [])
+      setAllTasks(response.data?.tasks?.length > 0 ? response.data.tasks : []);
 
       // Map statusSummary data with fixed labels and order
       const statusSummary = response.data?.statusSummary || {};
@@ -33,9 +33,9 @@ const ManageTasks = () => {
         { label: "Pending", count: statusSummary.pendingTasks || 0 },
         { label: "In Progress", count: statusSummary.inProgressTasks || 0 },
         { label: "Completed", count: statusSummary.completedTasks || 0 },
-      ]
+      ];
 
-      setTabs(statusArray)
+      setTabs(statusArray);
     } catch (error) {
       console.error("Error fetching users", error);
     }
@@ -53,22 +53,23 @@ const ManageTasks = () => {
     return () => {};
   }, [filterStatus]);
 
-
   return (
     <DashboardLayout activeMenu="Manage Tasks">
       <div className="my-5">
         <div className="flex flex-col lg:flex-row lg:items-center justify-between">
           <div className="flex items-center justify-between gap-3">
-            <h2 className="text-xl md:text-xl font-medium">My Tasks</h2>
+            <h2 className="text-xl md:text-xl font-semibold">My Tasks</h2>
 
-            <button className="flex lg:hidden download-btn" onClick={handleDownloadReport}>
+            <button
+              className="flex lg:hidden download-btn"
+              onClick={handleDownloadReport}
+            >
               <LuFileSpreadsheet className="text-lg" />
               Download Report
             </button>
-
           </div>
 
-          {allTasks?.length > 0 && (
+          {tabs?.[0]?.count > 0 && (
             <div className="flex items-center gap-3">
               <TaskStatusTabs
                 tabs={tabs}
@@ -76,11 +77,36 @@ const ManageTasks = () => {
                 setActiveTab={setFilterStatus}
               />
 
-              <button className="hidden lg:flex download-btn" onClick={handleDownloadReport}>
+              <button
+                className="hidden lg:flex download-btn"
+                onClick={handleDownloadReport}
+              >
                 <LuFileSpreadsheet className="text-lg" />
               </button>
             </div>
           )}
+        </div>
+
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mt-4">
+          {allTasks?.map((item, index) => (
+            <TaskCard
+              key={item._id}
+              title={item.title}
+              description={item.description}
+              priority={item.description}
+              status={item.status}
+              progress={item.progress}
+              createdAt={item.createdAt}
+              dueDate={item.dueDate}
+              assignedTo={item.assignedTo?.map((item) => item.profileImageUrl)}
+              attachmentCount={item.attachments?.length || 0}
+              completedTodoCount={item.completedTodoCount || 0}
+              todoChecklist={item.todoChecklist || []}
+              onClick={() => {
+                handleClick(item);
+              }}
+            />
+          ))}
         </div>
       </div>
     </DashboardLayout>
